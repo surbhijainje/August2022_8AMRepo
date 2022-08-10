294c647635d19484e0aae875a8357b03a554849b
ravilella1234 <masters.selenium@gmail.com>
2022-08-10 08:59:43 +0530

    Properties File

--- August2022_8AM_SeleniumBatch/src/test/java/com/launcher/BaseTest.java
+++ August2022_8AM_SeleniumBatch/src/test/java/com/launcher/BaseTest.java
@@ -1,5 +1,9 @@
 package com.launcher;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Properties;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -10,25 +14,39 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 public class BaseTest 
 {
 	public static WebDriver driver;
+	public static String projectPath = System.getProperty("user.dir");
+	public static Properties p;
+	
+	
+	public static void init() throws Exception
+	{
+		//FileInputStream fis = new FileInputStream("D:\\jan2022WD\\August2022_8AM_SeleniumBatch\\src\\test\\resources\\data.properties");
+		System.out.println(projectPath);
+		FileInputStream fis = new FileInputStream(projectPath+"\\src\\test\\resources\\data.properties");
+		p = new Properties();
+		p.load(fis);
+		//String e = p.getProperty("firefoxbrowser");
+		//System.out.println(e);
+	}
 	
 	public static void launch(String browser)
 	{
-		if(browser.equals("chrome")) {
+		if(p.getProperty(browser).equals("chrome")) {
 			WebDriverManager.chromedriver().setup();
 			driver = new ChromeDriver();
-		}else if(browser.equals("firefox")) {
+		}else if(p.getProperty(browser).equals("firefox")) {
 			WebDriverManager.firefoxdriver().setup();
 			driver = new FirefoxDriver();
-		}else if(browser.equals("ie")) {
+		}else if(p.getProperty(browser).equals("ie")) {
 			WebDriverManager.iedriver().setup();
 			driver = new InternetExplorerDriver();
 		}
 			
 	}
 	
-	public static void navigate(String url)
+	public static void navigateUrl(String url)
 	{
-		driver.get(url);
+		driver.get(p.getProperty(url));
 	}
 
 }
--- August2022_8AM_SeleniumBatch/src/test/java/com/launcher/TC_001.java
+++ August2022_8AM_SeleniumBatch/src/test/java/com/launcher/TC_001.java
@@ -3,11 +3,13 @@ package com.launcher;
 public class TC_001 extends BaseTest
 {
 
-	public static void main(String[] args) 
+	public static void main(String[] args) throws Exception 
 	{
-		launch("chrome");
+		init();
 		
-		navigate("https://www.amazon.in");
+		launch("chromebrowser");
+		
+		navigateUrl("amazonurl");
 
 	}
 

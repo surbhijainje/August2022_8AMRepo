4fb0ed7d6fdbdf7b2fc099bc6d572af8368445e8
ravilella1234 <masters.selenium@gmail.com>
2022-08-23 10:02:11 +0530

    DataTypes & its default values

---
+++ August2022_9AM_JavaBatch_Latest/src/icici/loans/homeloans/WapperClassDemo.java
@@ -0,0 +1,18 @@
+package icici.loans.homeloans;
+
+import java.util.ArrayList;
+
+public class WapperClassDemo 
+{
+
+	public static void main(String[] args) 
+	{
+		int x1 = 100;
+		System.out.println(x1);
+
+		
+		Integer x2 = 200;
+		System.out.println(x2);		
+	}
+
+}
